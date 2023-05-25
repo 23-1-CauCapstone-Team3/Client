5def1585368393077210b1984dc0344c482f11a3
Heeming <purete27@gmail.com>
2023-05-25 20:02:55 +0900

    [FEAT ADD] Add current_route_page

--- lib/app/modules/views/current_route_page.dart
+++ lib/app/modules/views/current_route_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:last_transport/app/modules/views/lock_screen_activity_page.dart';
+
+import '../../data/theme_data.dart';
 
 class CurrentRoutePage extends StatefulWidget {
   const CurrentRoutePage({Key? key}) : super(key: key);
@@ -7,12 +10,40 @@ class CurrentRoutePage extends StatefulWidget {
   _CurrentRoutePageState createState() => _CurrentRoutePageState();
 }
 
-class _CurrentRoutePageState extends State<CurrentRoutePage> {
+class _CurrentRoutePageState extends State<CurrentRoutePage> with AutomaticKeepAliveClientMixin {
+
+  int _selectedIndex = 1;
+
+  final List<Widget> pages = <Widget>[LockScreenActivityPage()];
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final pageController = PageController(initialPage: 1);
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  void initState() {
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('This is a Current Route Page'),
+
+    return Scaffold(
+      backgroundColor: CustomColors.pageBackgroundColor,
+      body: PageView(
+        controller: pageController,
+        onPageChanged: _onPageChanged,
+        physics: const NeverScrollableScrollPhysics(),
+        children: pages,
+      ),
     );
+
   }
 }
